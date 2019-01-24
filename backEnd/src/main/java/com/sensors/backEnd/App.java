8231a9e52abbb4d4493545d444e625c22fdf62b0
Roshixa <abidsoukayna@gmail.com>
2019-01-24 03:26:22 +0000

    Add inserting into database to main app

--- backEnd/src/main/java/com/sensors/backEnd/App.java
+++ backEnd/src/main/java/com/sensors/backEnd/App.java
@@ -1,9 +1,11 @@
 package com.sensors.backEnd;
 
+import com.sensors.backEnd.database.JDBCDataDAO;
 import com.sensors.backEnd.readData.*;
 
 public class App {
 	public static void main(String[] args) {
+		JDBCDataDAO dataJ = new JDBCDataDAO();
 		LuminosityMoisiture luminosityMoisiture = new LuminosityMoisiture();
 		TemperatureHumidity temperatureHumidity = new TemperatureHumidity();
 		temperatureHumidity.read();
@@ -13,5 +15,7 @@ public class App {
 				luminosityMoisiture.getMoisiture(),
 				luminosityMoisiture.getLuminosity());
 		System.out.println(data);
+		dataJ.createNewTable();
+		dataJ.insert(data);
 	}
 }
