f8c818118ea035977280a677051b799b29ff9c7d
Roshixa <abidsoukayna@gmail.com>
2019-01-24 02:07:34 +0000

    Modify constructor of Data and add a toString method

--- backEnd/src/main/java/com/sensors/backEnd/Data.java
+++ backEnd/src/main/java/com/sensors/backEnd/Data.java
@@ -3,26 +3,31 @@ package com.sensors.backEnd;
 import java.util.Date;
 
 public class Data {
-	
 	Integer id;
 	Double temperature;
 	Double humidity;
 	Double moisiture;
 	Double luminosity;
 	Date date;
-	
-	public Data(Integer id, Double temperature, Double humidity,
-			Double moisiture, Double luminosity, Date date) {
-		this.id = id;
+
+	public Data(Double temperature, Double humidity, Double moisiture,
+			Double luminosity) {
 		this.temperature = temperature;
 		this.humidity = humidity;
 		this.moisiture = moisiture;
 		this.luminosity = luminosity;
-		this.date = date;
+		this.date = new Date();
 	}
 
 	public Data() {
 		super();
 	}
-	
+
+	@Override
+	public String toString() {
+		return "Data [temperature=" + temperature + ", humidity=" + humidity
+				+ ", moisiture=" + moisiture + ", luminosity=" + luminosity
+				+ ", date=" + date + "]";
+	}
+
 }
