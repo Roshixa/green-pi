6cd702c17a4b5ec22d843aaf90dc3edeecf30be6
Roshixa <abidsoukayna@gmail.com>
2019-01-27 00:33:55 +0100

    Implement methods of String Utils class

--- server/src/main/utils/StringUtils.java
+++ server/src/main/utils/StringUtils.java
@@ -1,12 +1,43 @@
 package main.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public final class StringUtils {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private StringUtils() {
     }
 
-    public static final String encrypt(String str){
-        return str;
+    public static final String encrypt(String str) {
+        if (str == null) {
+            return null;
+        }
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            String hash = new BigInteger(1, md.digest()).toString(16);
+            while (hash.length() < 32) {
+                hash = "0" + hash;
+            }
+            return hash;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
+    public static final String toJSON(Object obj) {
+        try {
+            return MAPPER.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
