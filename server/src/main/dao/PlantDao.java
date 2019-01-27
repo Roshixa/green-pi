8c878ee58edfa0b5415416a7b03eb9adfcb55085
Roshixa <abidsoukayna@gmail.com>
2019-01-27 04:04:51 +0100

    Implement Plant DAO interface

--- server/src/main/dao/PlantDao.java
+++ server/src/main/dao/PlantDao.java
@@ -1,4 +1,14 @@
 package main.dao;
 
+import main.beans.Plant;
+import main.beans.User;
+
+import java.util.List;
+
 public interface PlantDao {
+    Plant find(Integer id);
+
+    List<Plant> find(User user);
+
+    boolean save(Plant plant);
 }
