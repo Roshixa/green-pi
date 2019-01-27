9a1c5187a426219563d9aa040288f3a4db3e5ff7
Roshixa <abidsoukayna@gmail.com>
2019-01-27 21:31:42 +0100

    Implement tests for Raspberry Service

--- server/src/main/service/RaspberryService.java
+++ server/src/main/service/RaspberryService.java
@@ -1,4 +1,12 @@
 package main.service;
 
+import main.beans.Plant;
+
 public interface RaspberryService {
+    Boolean checkConnection(Plant plant);
+
+    String getLastData(Plant plant);
+
+    String geAllLastData(Plant plant);
+
 }
