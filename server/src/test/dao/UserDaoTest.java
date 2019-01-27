a9517ace3523638a234497ac3a6c3493fad5f5c9
Roshixa <abidsoukayna@gmail.com>
2019-01-27 16:17:57 +0100

    Add static fields in User DAO Test

--- server/src/test/dao/UserDaoTest.java
+++ server/src/test/dao/UserDaoTest.java
@@ -12,7 +12,9 @@ import java.util.Random;
 import static org.junit.Assert.*;
 
 public class UserDaoTest {
-    private static final String EXISTANT_USER = "a@gmail.com";
+    private static final String EXISTANT_USER_EMAIL = "a@gmail.com";
+    private static final String EXISTANT_USER_PASSWORD = "pass";
+    private static final String USER_NAME = "_test";
     private static UserDao userDao;
 
     @BeforeClass
@@ -27,19 +29,19 @@ public class UserDaoTest {
 
     @Test
     public void findUser() {
-        User user = userDao.find(EXISTANT_USER);
+        User user = userDao.find(EXISTANT_USER_EMAIL);
         assertNotNull(user);
 
-        user = userDao.find(EXISTANT_USER + new Random().nextInt());
+        user = userDao.find(EXISTANT_USER_EMAIL + new Random().nextInt());
         assertNull(user);
     }
 
     @Test
     public void saveUser() {
-        User user = new User(EXISTANT_USER, " ", " ");
+        User user = new User(EXISTANT_USER_EMAIL, EXISTANT_USER_PASSWORD, USER_NAME);
         assertFalse(userDao.save(user));
 
-        user = new User(new Random().nextInt() + EXISTANT_USER + "_test", " ", " ");
+        user = new User(new Random().nextInt() + EXISTANT_USER_EMAIL + "_test", " ", " ");
         assertTrue(userDao.save(user));
     }
 }
