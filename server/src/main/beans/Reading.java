035d10058cce898257ea9f67df00360191179304
Roshixa <abidsoukayna@gmail.com>
2019-01-27 20:31:36 +0100

    Add beans for reading and readings

---
+++ server/src/main/beans/Reading.java
@@ -0,0 +1,60 @@
+package main.beans;
+
+public class Reading {
+    private Double luminosity;
+    private Double humidity;
+    private Double moisture;
+    private Double temperature;
+
+    public Reading(Double luminosity, Double humidity, Double moisture, Double temperature) {
+        this.luminosity = luminosity;
+        this.humidity = humidity;
+        this.moisture = moisture;
+        this.temperature = temperature;
+    }
+
+    public Reading() {
+    }
+
+    public Double getLuminosity() {
+        return luminosity;
+    }
+
+    public void setLuminosity(Double luminosity) {
+        this.luminosity = luminosity;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
+
+    public Double getMoisture() {
+        return moisture;
+    }
+
+    public void setMoisture(Double moisture) {
+        this.moisture = moisture;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "Reading{" +
+                "luminosity=" + luminosity +
+                ", humidity=" + humidity +
+                ", moisture=" + moisture +
+                ", temperature=" + temperature +
+                '}';
+    }
+}
---
+++ server/src/main/beans/Readings.java
@@ -0,0 +1,62 @@
+package main.beans;
+
+import java.util.List;
+
+public class Readings {
+    private List<Double> luminosity;
+    private List<Double> humidity;
+    private List<Double> moisture;
+    private List<Double> temperature;
+
+    public Readings(List<Double> luminosity, List<Double> humidity, List<Double> moisture, List<Double> temperature) {
+        this.luminosity = luminosity;
+        this.humidity = humidity;
+        this.moisture = moisture;
+        this.temperature = temperature;
+    }
+
+    public Readings() {
+    }
+
+    public List<Double> getLuminosity() {
+        return luminosity;
+    }
+
+    public void setLuminosity(List<Double> luminosity) {
+        this.luminosity = luminosity;
+    }
+
+    public List<Double> getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(List<Double> humidity) {
+        this.humidity = humidity;
+    }
+
+    public List<Double> getMoisture() {
+        return moisture;
+    }
+
+    public void setMoisture(List<Double> moisture) {
+        this.moisture = moisture;
+    }
+
+    public List<Double> getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(List<Double> temperature) {
+        this.temperature = temperature;
+    }
+
+    @Override
+    public String toString() {
+        return "Readings{" +
+                "luminosity=" + luminosity +
+                ", humidity=" + humidity +
+                ", moisture=" + moisture +
+                ", temperature=" + temperature +
+                '}';
+    }
+}
