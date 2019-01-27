4d01242310bba4d558670cae3cab6864ed953a4c
Roshixa <abidsoukayna@gmail.com>
2019-01-27 16:52:08 +0100

    Implement doGet method in Home Servlet

--- server/src/main/servlets/HomeServlet.java
+++ server/src/main/servlets/HomeServlet.java
@@ -1,6 +1,8 @@
 package main.servlets;
 
-import javax.servlet.ServletException;
+import main.service.UserService;
+import main.service.impl.UserServiceImpl;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +11,13 @@ import java.io.IOException;
 
 @WebServlet("/")
 public class HomeServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final UserService userService = new UserServiceImpl();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        if (!userService.hasLoggedIn(request.getSession())) {
+            response.sendRedirect(request.getContextPath() + "/login");
+        } else {
+            response.sendRedirect(request.getContextPath() + "/add");
+        }
     }
 }
