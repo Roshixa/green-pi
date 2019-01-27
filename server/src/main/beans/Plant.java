5d72288f8c82ba1f6ea6c29772245ab7e121ed1c
Roshixa <abidsoukayna@gmail.com>
2019-01-27 04:04:21 +0100

    Add other fields to plant class

--- server/src/main/beans/Plant.java
+++ server/src/main/beans/Plant.java
@@ -6,39 +6,89 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 public class Plant {
     private Integer id;
     private Integer userId;
+    private String dbAddress;
+    private String dbUsername;
+    private String dbPassword;
+    private Integer dbPort;
     private String name;
-    private String address;
     private String description;
 
-    public Plant(Integer id, Integer userId, String name, String address, String description) {
-        this.id = id;
-        this.userId = userId;
-        this.name = name;
-        this.address = address;
-        this.description = description;
+    public Plant() {
     }
 
-    public Plant() {
+    public Plant(Integer id, Integer userId, String dbAddress, String dbUsername, String dbPassword, Integer dbPort, String name, String description) {
+        this.id = id;
+        this.userId = userId;
+        this.dbAddress = dbAddress;
+        this.dbUsername = dbUsername;
+        this.dbPassword = dbPassword;
+        this.dbPort = dbPort;
+        this.name = name;
+        this.description = description;
     }
 
     public Integer getId() {
         return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public Integer getUserId() {
         return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getDbAddress() {
+        return dbAddress;
+    }
+
+    public void setDbAddress(String dbAddress) {
+        this.dbAddress = dbAddress;
+    }
+
+    public String getDbUsername() {
+        return dbUsername;
+    }
+
+    public void setDbUsername(String dbUsername) {
+        this.dbUsername = dbUsername;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+
+    public void setDbPassword(String dbPassword) {
+        this.dbPassword = dbPassword;
+    }
+
+    public Integer getDbPort() {
+        return dbPort;
+    }
+
+    public void setDbPort(Integer dbPort) {
+        this.dbPort = dbPort;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getAddress() {
-        return address;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     @Override
@@ -46,8 +96,11 @@ public class Plant {
         return "Plant{" +
                 "id=" + id +
                 ", userId=" + userId +
+                ", dbAddress='" + dbAddress + '\'' +
+                ", dbUsername='" + dbUsername + '\'' +
+                ", dbPassword='" + dbPassword + '\'' +
+                ", dbPort=" + dbPort +
                 ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
                 ", description='" + description + '\'' +
                 '}';
     }
