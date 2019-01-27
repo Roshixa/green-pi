4ba25d862f4e9a0eb64f46ad08d16cd0f13a200d
Roshixa <abidsoukayna@gmail.com>
2019-01-27 02:54:43 +0100

    Implement database connection (H2 & MySQL)

--- server/src/main/utils/DbConnectionsUtils.java
+++ server/src/main/utils/DbConnectionsUtils.java
@@ -1,7 +1,38 @@
 package main.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public final class DbConnectionsUtils {
 
     private DbConnectionsUtils() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static Connection createH2Connection(String hostname) {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(hostname);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static Connection createMySqlConnection(String hostname, String user, String password) {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(hostname, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
 }
