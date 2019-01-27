8cf54110382f2eca5b046f3b2fc8c195567527b8
Roshixa <abidsoukayna@gmail.com>
2019-01-27 19:42:28 +0100

    Implement plant service

--- server/src/main/service/impl/PlantServiceImpl.java
+++ server/src/main/service/impl/PlantServiceImpl.java
@@ -1,16 +1,28 @@
 package main.service.impl;
 
 import main.beans.Plant;
+import main.dao.PlantDao;
+import main.dao.h2.PlantDaoImpl;
 import main.service.PlantService;
 
+import static main.utils.StringUtils.encrypt;
+
 public class PlantServiceImpl implements PlantService {
+    private PlantDao plantDao;
+
+    public PlantServiceImpl() {
+        this.plantDao = new PlantDaoImpl();
+    }
+
     @Override
     public boolean add(String userEmail, String dbAddress, String dbUsername, String dbPassword, Integer dbPort, String name, String description) {
-        return false;
+        String hash = encrypt(dbAddress + dbPort);
+        Plant plant = new Plant(userEmail, dbAddress, dbUsername, dbPassword, dbPort, name, description, hash);
+        return plantDao.save(plant);
     }
 
     @Override
     public Plant find(String dbAddress, Integer port) {
-        return null;
+        return plantDao.find(dbAddress,port);
     }
 }
