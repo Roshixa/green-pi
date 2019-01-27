f5006a97548caaac3b8145c85680ed88a6a773db
Roshixa <abidsoukayna@gmail.com>
2019-01-27 16:21:56 +0100

    Add login conditions to login servlet, and methods to user service

--- server/src/main/service/UserService.java
+++ server/src/main/service/UserService.java
@@ -1,8 +1,18 @@
 package main.service;
 
+import main.beans.User;
+
+import javax.servlet.http.HttpSession;
+
 public interface UserService {
 
-    boolean login(String email, String password);
+    boolean login(String email, String password, HttpSession session);
 
     boolean signUp(String name, String email, String password, String passwordConfirmation);
+
+    User getSessionUser(HttpSession httpSession);
+
+    void saveSessionUser(User user, HttpSession httpSession);
+
+    boolean hasLoggedIn(HttpSession httpSession);
 }
--- server/src/main/servlets/LoginServlet.java
+++ server/src/main/servlets/LoginServlet.java
@@ -1,6 +1,7 @@
 package main.servlets;
 
 import main.service.UserService;
+import main.service.impl.UserServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,25 +10,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static main.utils.StringUtils.encrypt;
-
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-    private UserService userService;
+    private static final UserService userService = new UserServiceImpl();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String email = request.getParameter("email");
-        String password = encrypt(request.getParameter("password"));
-        if (request.getParameter("name") != null) {
-            String name = request.getParameter("name");
-            String passwordConfirmation = request.getParameter("password_confirmation");
-            userService.signUp(name, email, password, passwordConfirmation);
+        if (!userService.hasLoggedIn(request.getSession())) {
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            if (request.getParameter("name") != null) {
+                String name = request.getParameter("name");
+                String passwordConfirmation = request.getParameter("password_confirmation");
+                userService.signUp(name, email, password, passwordConfirmation);
+            }
+            userService.login(email, password, request.getSession());
         }
-        userService.login(email, password);
         response.sendRedirect(request.getContextPath() + "/");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/layout/login.jsp").forward(request, response);
+        if (!userService.hasLoggedIn(request.getSession())) {
+            request.getRequestDispatcher("WEB-INF/layout/login.jsp").forward(request, response);
+        } else {
+            response.sendRedirect(request.getContextPath() + "/");
+        }
     }
 }
